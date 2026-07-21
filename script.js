e024d4caa54f2bdd51f6752aa998f2c7b551fdec
Joshua Karanja <jokaranja26@gmail.com>
2026-07-21 07:31:58 +0300

    learnt if else statement

---
+++ script.js
@@ -0,0 +1,76 @@
+// 
+// let js = ' javascript';
+
+// 40+8+32-10
+// console.log(40+8+32-10);
+// console.log(23);
+
+// let firstName = 'Julia';
+// console.log(firstName);
+
+// let myFirstJob='programmer'
+// console.log(myFirstJob);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+// console.log(typeof null);
+
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// // birthYear = 1990;
+
+// const job;
+// const now = 2026;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2001;
+
+// console.log(now - 1991 > now -2001);
+
+// console.log(ageJonas , ageSarah)
+
+const firstName = 'Josh';
+const job = 'full stack developer';
+const birthYear = 2001;
+const year = 2026;
+// console.log (firstName + ' ' + lastName);
+
+// let x = 10+5;
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+// console.log(x);
+
+// console.log(ageJonas > ageSarah);
+
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.92;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn , markHigherBMI);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+console.log(jonasNew)
+
+const age = 15;
+
+
+if(age >= 18){
+    console.log('Joshua can start driving);')
+}else{
+    const yearsLeft = 18 -age;
+    console.log(`Joshua is too young. Wait another ${yearsLeft} years :)`);
+}
